e2738ed149db47acf3f45f762156aaee9a19c40d
Daniel Knopf <danielkn609@gmail.com>
2021-04-23 20:54:07 -0400

    added firestore and firebase to package.json

---
+++ home/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View } from "react-native";
+import App from "../App";
+
+function Home(props) {
+  return (
+    <View>
+      <App />
+    </View>
+  );
+}
