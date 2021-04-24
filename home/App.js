fb1eda31f1df6ab07334669f08173add03bc66f1
Nalu Tripician <nalutrip@icloud.com>
2021-04-23 21:41:50 -0400

    imagePicker

--- home/App.js
+++ home/App.js
@@ -1,12 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import openImagePickerAsync from './imagePicker';
 
 export default function App() {
+  let [selectedImage, setSelectedImage] = React.useState(null);
+
+  if (selectedImage !== null) {
+    return (
+      <View style={styles.container}>
+        <Image source={{ uri: selectedImage.localUri }} style={styles.thumbnail} />
+        </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Daniel Smells like Beans</Text>
-      <StatusBar style="auto" />
+      <Image source={{ uri: 'https://i.imgur.com/TkIrScD.png' }} style={styles.logo} />
+      <Text style={styles.instructions}>
+        To share a photo from your phone with a friend, just press the button below!
+      </Text>
+
+      <Pressable
+       hitSlop={30}
+        onPress={ async () => {
+            let image = await openImagePickerAsync();
+            image ? setSelectedImage(image) : null
+            }
+        }
+        style={styles.button}>
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </Pressable>
     </View>
   );
 }
---
+++ home/imagePicker.js
@@ -0,0 +1,17 @@
+import * as ImagePicker from 'expo-image-picker';
+
+export default async function openImagePickerAsync() {
+    let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert('Permission to access camera roll is required!');
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+    return ({localUri: pickerResult.uri });
+};
